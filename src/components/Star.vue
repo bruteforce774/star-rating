ee13f1f0c0e8eabfb7aeaa126155d707c9c2f56e
Daniel Andreas Wang <codingforpeace@protonmail.com>
2026-05-12 22:24:40 +0200

    add minimal star component

--- src/components/Star.vue
+++ src/components/Star.vue
@@ -1,5 +1,12 @@
 <script setup>
+defineProps({
+  highlighted: Boolean
+})
+const emit = defineEmits(['clicked'])
 </script>
 
 <template>
+  <span @click="emit('clicked')">
+    {{ highlighted ? '★' : '☆' }}
+  </span>
 </template>
